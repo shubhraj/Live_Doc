4a4e2f3ae8d7bc18e66ea4c135fbbc489b8e5456
Shubham patil <shubham.patil@sincrodigital.com>
2024-08-23 14:38:28 +0530

    integrate clerk for authentication

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Documents = () => {
@@ -7,8 +8,14 @@ const Documents = () => {
     <div>
       <Header>
         <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='document-title'> This is Fake Title</p>
+          <p className='document-title'> Share </p>
         </div>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
       <Editor />
     </div>
